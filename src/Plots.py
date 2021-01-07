d4d0a1e4f67a76d44d03d46a5e534a3da63b77d3
Foppe <foppecrezee@hotmail.com>
2021-01-07 17:44:52 +0100

    added opacity setting for plot_two_same_axis

--- src/Plots.py
+++ src/Plots.py
@@ -7,4 +7,4 @@ data1 = stats.get_average_reward_simulation()
 # stats.plot_data(stats.get_average_reward_simulation())
 # stats.plot_data(stats.get_data_rolling_window(5))
 stats.plot_two_same_axis(stats.get_average_reward_simulation(), stats.get_data_rolling_window(5), label1="avg reward",
-                         label2="avg reward with a window of size 5")
+                         label2="avg reward with a window of size 5", opacity_n=1)
