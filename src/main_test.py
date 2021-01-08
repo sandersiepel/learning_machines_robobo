1b91db4581dc7e07bfd7e7fe39f66d5dc89ec2aa
sandersiepel <Qnwzcs2qw@@>
2021-01-08 15:24:07 +0100

    improved the code and deleted the distribution functions (not needed anymore, we know the distributions)

--- src/main_test.py
+++ src/main_test.py
@@ -20,7 +20,7 @@ from tqdm import tqdm, trange
 # set MODE = "train". If you use "train" you can select either NEW_Q_TABLE = True or False in the Environment class.
 MODE = "train"
 #TODO change to include test name
-TEST_FILENAME = "results/q_table_50_200_name.pickle"  # Structure should be /src/results.
+TEST_FILENAME = "results/reward_data_20_150_dynamic_epsilon.pickle"  # Structure should be /src/results.
 
 
 class Direction:
@@ -33,14 +33,14 @@ class Direction:
 
 class Environment:
     # All of our constants, prone to change.
-    MAX_ITERATIONS = 50  # Amount of simulations until termination.
-    MAX_SIMULATION_ITERATIONS = 200  # Amount of actions within one simulation. Actions = Q-table updates.
+    MAX_ITERATIONS = 100  # Amount of simulations until termination.
+    MAX_SIMULATION_ITERATIONS = 300  # Amount of actions within one simulation. Actions = Q-table updates.
     LEARNING_RATE = .1
     DISCOUNT_FACTOR = .95
     NEW_Q_TABLE = True  # True if we want to start new training, False if we want to use existing file.
-    EXPERIMENT_NAME = 'dynamic epsilon'
+    EXPERIMENT_NAME = 'throwaway'
     FILENAME = f"results/reward_data_{MAX_ITERATIONS}_{MAX_SIMULATION_ITERATIONS}_{EXPERIMENT_NAME}.pickle"  # Name of the q-table in case we LOAD the data (for testing).
-    IP_ADDRESS = os.environ['IP_ADRES']
+    IP_ADDRESS = os.environ['IP_ADDRESS']
 
     EPSILON_LOW = .6  # Start epsilon value. This gradually increases.
     EPSILON_HIGH = .99  # End epsilon value
@@ -64,11 +64,35 @@ class Environment:
         self.rob = robobo.SimulationRobobo().connect(address=self.IP_ADDRESS, port=19997)
         self.stats = Statistics(self.MAX_ITERATIONS, self.MAX_SIMULATION_ITERATIONS)
 
-        # Start with either a new Q-table or load one
         if self.NEW_Q_TABLE:
             self.q_table = self.initialize_q_table()
         else:
             self.q_table = self.read_q_table(self.FILENAME)
+
+    def start_environment(self):
+        for i in trange(self.MAX_ITERATIONS):  # Nifty, innit?
+            # print(f"Starting simulation nr. {i+1}/{self.MAX_ITERATIONS}. Epsilon: {self.EPSILON_LOW}. Q-table size: {self.q_table.size}")
+
+            self.rob.play_simulation()
+            # A simulation runs until valid_environment returns False.
+            while self.valid_environment():
+                curr_state = self.handle_state()  # Check in what state rob is, return tuple e.g. (0, 0, 0, 1, 0)
+
+                # Do we perform random action (due to epsilon < 1) or our best possible action?
+                best_action = self.determine_action(curr_state)
+
+                # Given our selected action (whether best or random), perform this action and update the Q-table.
+                reward = self.update_q_table(best_action, curr_state)
+                self.stats.add_reward(i, self.iteration_counter, reward)  # Add the reward for visualization purposes.
+                self.change_epsilon()  # Check if we should increase epsilon or not.
+                self.iteration_counter += 1  # Keep track of how many actions this simulation does.
+            else:
+                # print(f"Environment is not valid anymore, starting new environment")
+                self.store_q_table()  # Save Q-table after each iteration because, why not.
+                self.iteration_counter = 0
+                self.collision_counter = 0
+                self.rob.stop_world()
+                self.rob.wait_for_ping()  # Maybe we should wait for ping so we avoid errors. Might not be necessary.
 
     @staticmethod
     def read_q_table(filename):
@@ -80,35 +104,8 @@ class Environment:
         with open(f"results/q_table_{self.MAX_ITERATIONS}_{self.MAX_SIMULATION_ITERATIONS}.pickle", 'wb') as fp:
             pickle.dump(self.q_table, fp)
 
-    def start_environment(self):
-        for i in trange(self.MAX_ITERATIONS):  # Nifty, innit?
-            current_time = datetime.now().strftime("%H:%M:%S")
-            print(f"Starting simulation nr. {i+1}/{self.MAX_ITERATIONS}. Epsilon: {self.EPSILON_LOW}. Current time: {current_time}")
-
-            self.rob.play_simulation()
-            # A simulation runs until valid_environment returns False.
-            while self.valid_environment():
-                curr_state = self.handle_state()  # Check in what state rob is, return tuple e.g. (0, 0, 0, 1, 0)
-                self.state_distribution.append(curr_state)  # Temporarily keep track of all used states for state distribution.
-                # TODO: after fixing state distribution, remove this part.
-
-                # Do we perform random action (due to epsilon < 1) or our best possible action?
-                if random.random() < (1 - self.EPSILON_LOW):
-                    best_action = random.choice(self.action_space)
-                else:
-                    best_action = np.argmax(self.q_table[curr_state])
-
-                # Given our selected action (whether best or random), perform this action and update the Q-table.
-                reward = self.update_q_table(best_action, curr_state)
-                self.stats.add_reward(i, self.iteration_counter, reward)
-                self.change_epsilon()  # Check if we should increase epsilon or not.
-                self.iteration_counter += 1  # Keep track of how many actions this simulation does.
-            else:
-                print(f"Environment is not valid anymore, starting new environment")
-                self.store_q_table()
-                self.iteration_counter = 0
-                self.rob.stop_world()
-                self.rob.wait_for_ping()  # Maybe we should wait for ping so we avoid errors.
+    def determine_action(self, curr_state):
+        return random.choice(self.action_space) if random.random() < (1 - self.EPSILON_LOW) else np.argmax(self.q_table[curr_state])
 
     def test(self, filename):
         # This function can be used to test a Q-table. It will simply run the environment with a deterministic policy
@@ -116,6 +113,7 @@ class Environment:
         # Terminate this function by pressing CTRL + C in terminal.
         self.rob.play_simulation()
         self.read_q_table(filename)
+
         while True:
             curr_state = self.handle_state()  # Check in what state rob is, return tuple e.g. (0, 0, 0, 1, 0)
             best_action = np.argmax(self.q_table[curr_state])  # Choose its best action (deterministic).
@@ -129,12 +127,10 @@ class Environment:
 
     def valid_environment(self):
         # This function checks whether the current simulation can continue or not, depending on several criteria.
-        if self.collision_counter > self.COLLISION_THRESHOLD:
-            return False
-        if self.iteration_counter >= self.MAX_SIMULATION_ITERATIONS:
-            return False
+        c1 = self.collision_counter > self.COLLISION_THRESHOLD
+        c2 = self.iteration_counter >= self.MAX_SIMULATION_ITERATIONS
 
-        return True
+        return False if not c1 and c2 else True
 
     def change_epsilon(self):
         # This function changes the epsilon value if needed. Only does so if we did x amount of iterations, and the
@@ -142,7 +138,7 @@ class Environment:
         if self.epsilon_counter == self.epsilon_increase:
             if self.EPSILON_LOW < self.EPSILON_HIGH:
                 self.EPSILON_LOW += self.EPSILON_INCREASE
-                print(f"Increasing epsilon to {self.EPSILON_LOW}")
+                # print(f"Increasing epsilon to {self.EPSILON_LOW}")
                 self.epsilon_counter = 0
         else:
             self.epsilon_counter += 1
@@ -152,8 +148,7 @@ class Environment:
         # Since observation space is very large, we need to trim it down (bucketing) to only a select amount of
         # possible states, e.g. 4 for each sensor (4^8 = 65k). Or: use less sensors (no rear sensors for task 1).
         # The size (5, 5, 5, 5, 5) denotes each sensor, with its amount of possible states (see func handle_state).
-        q_table = np.random.uniform(low=0, high=0, size=([3, 3, 3, 3, 3] + [len(self.action_space)]))
-        return np.round(q_table)
+        return np.random.uniform(low=0, high=0, size=([3, 3, 3, 3, 3] + [len(self.action_space)]))
 
     def handle_state(self):
         # This function should return the values with which we can index our q_table, in tuple format.
@@ -161,26 +156,21 @@ class Environment:
         # the bucket size is already determined by the shape of the q_table.
         try:
             sensor_values = np.log(np.array(self.rob.read_irs())[3:]) / 10
-        except:
+        except RuntimeWarning:
             sensor_values = [0, 0, 0, 0, 0]
+
         sensor_values = np.where(sensor_values == -np.inf, 0, sensor_values)  # Remove the infinite values.
         sensor_values = (sensor_values - -0.65) / 0.65  # Scales all variables between [0, 1] where 0 is close proximity.
 
         # Check what the actual sensor_values are (between [0, 1]) and determine their state
-        # TODO fix distribution
         indices = []
         for sensor_value in sensor_values:
-            self.state_distribution2.append(round(sensor_value, 3))
             if sensor_value >= 0.8:  # No need for action, moving forward is best.
                 indices.append(0)
             elif 0.65 <= sensor_value < 0.8:
                 indices.append(1)
             elif sensor_value < 0.65:  # We see an object, but not really close yet.
                 indices.append(2)
-            # elif 0.2 <= sensor_value < 0.4:
-            #     indices.append(3)
-            # elif 0 <= sensor_value < 0.2:  # Close proximity.
-            #     indices.append(4)
 
         # Return the values in tuple format, with which we can index our Q-table. This tuple is a representation
         # of the current state our robot is in (i.e. what does the robot see with its sensors).
@@ -191,9 +181,7 @@ class Environment:
         # It returns two things: new_state, which is the state (in tuple format) after this action has been performed.
         # and reward, which is the reward from this action.
         reward = -1
-
-        # Keep track of collision.
-        collision = self.collision()
+        collision = self.collision()  # Do we collide, True/False
 
         if action == 0:
             left, right, duration = Direction.LEFT  # Left, action 0
@@ -211,7 +199,6 @@ class Environment:
                 reward += 2
 
         self.rob.move(left, right, duration)
-
         return self.handle_state(), reward  # New_state, reward
 
     def collision(self):
@@ -220,15 +207,14 @@ class Environment:
         # then the environment should reset (to avoid rob getting stuck).
         try:
             sensor_values = self.rob.read_irs()[3:]  # Should be absolute values (no log or anything).
-        except:
+        except RuntimeWarning:
             sensor_values = [0, 0, 0, 0, 0]
-        collision = any([0 < i < self.collision_boundary for i in sensor_values])
 
+        collision = any([0 < i < self.collision_boundary for i in sensor_values])
         if collision:
             self.collision_counter += 1
             return True
         else:
-            self.collision_counter = 0
             return False
 
     def update_q_table(self, best_action, curr_state):
@@ -252,41 +238,7 @@ class Environment:
 
         # And lastly, update the value in the Q-table.
         self.q_table[curr_state][best_action] = new_q
-
         return reward
-
-    def calculate_state_distribution(self):
-        # TODO delete this function when it's not needed anymore.
-        data = self.state_distribution  # List of (state) tuples.
-        print("dist: ", data)
-        dict_count = {}
-        for a in self.action_space:
-            dict_count[a] = 0
-
-        for tup in data:
-            for sensor_state in tup:
-                dict_count[sensor_state] += 1
-
-        return dict_count
-
-    def calculate_state_distribution2(self):
-        # Calculates the density function of all 5 sensors
-        # TODO delete this function when it's not needed anymore.
-        data = self.state_distribution2
-        plt.rcParams.update({'figure.figsize': (7, 5), 'figure.dpi': 100})
-
-        # Plot Histogram on x
-        x = data
-        plt.hist(x, bins=50)
-        plt.yscale('log')
-        plt.gca().set(title='Frequency Histogram log', ylabel='Frequency')
-        plt.show()
-
-        x = data
-        plt.hist(x, bins=50)
-        plt.yscale('linear')
-        plt.gca().set(title='Frequency Histogram linear', ylabel='Frequency')
-        plt.show()
 
 
 def main():
@@ -294,8 +246,6 @@ def main():
     if MODE == "train":
         env.start_environment()
         env.stats.save_rewards(env.FILENAME)
-        # print(env.calculate_state_distribution())  # TODO: remove this
-        # env.calculate_state_distribution2()  # TODO: remove this
     elif MODE == "test":
         env.test(TEST_FILENAME)
 
