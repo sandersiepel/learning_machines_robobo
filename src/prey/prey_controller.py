42198f3094982243f950b8ccd68456cb40338bb7
sandersiepel <Qnwzcs2qw@@>
2021-01-26 09:34:59 +0100

    -

--- src/prey/prey_controller.py
+++ src/prey/prey_controller.py
@@ -2,6 +2,14 @@ import random
 import threading
 
 
+class Direction:
+    LEFT = (-5, 5, 300)  # Action: 0, left
+    RIGHT = (5, -5, 300)  # Action: 1, right
+    FORWARD = (25, 25, 300)  # Action: 2, forward
+    RRIGHT = (-15, 15, 300)  # Action: 3, strong right
+    LLEFT = (15, -15, 300)  # Action: 4, strong left
+
+
 class StoppableThread(threading.Thread):
     """Thread class with a stop() method. The thread itself has to check
         regularly for the stopped() condition."""
@@ -40,90 +48,7 @@ class Prey(StoppableThread):
         return [0 if value is False else (((value - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min for value in sensors_values]
 
     def run(self):
-        """
-        Method that moves the robot.
-        It avoids obstacles and with a predefined probability it changes direction
-        :return:
-        """
-        if self._log is not None:
-            self._log.info("The robot prey is moving")
-        else:
-            print("The robot prey is moving")
-        if self._level == 0:
-            if self._log is not None:
-                self._log.info("Level {} selected -> super easy option".format(self._level))
-            else:
-                print("Level {} selected -> super easy option".format(self._level))
-            maximum_speed = 5.0
-            turning_speed = 5.0
-            epsilon = 0.02
-        elif self._level == 1:
-            if self._log is not None:
-                self._log.info("Level {} selected -> easy option".format(self._level))
-            else:
-                print("Level {} selected -> easy option".format(self._level))
-            maximum_speed = 10.0
-            turning_speed = 10.0
-            epsilon = 0.03
-        elif self._level == 2:
-            if self._log is not None:
-                self._log.info("Level {} selected -> medium option".format(self._level))
-            else:
-                print("Level {} selected -> medium option".format(self._level))
-            maximum_speed = 20.0
-            turning_speed = 10.0
-            epsilon = 0.06
-        elif self._level == 3:
-            if self._log is not None:
-                self._log.info("Level {} selected -> hard option".format(self._level))
-            else:
-                print("Level {} selected -> hard option".format(self._level))
-            maximum_speed = 40.0
-            turning_speed = 20.0
-            epsilon = 0.08
-        elif self._level == 4:
-            if self._log is not None:
-                self._log.info("Level {} selected -> insane (Good luck Catching Me)".format(self._level))
-            else:
-                print("Level {} selected -> insane (Good luck Catching Me)".format(self._level))
-            maximum_speed = 70.0
-            turning_speed = 30.0
-            epsilon = 0.1
-        else:
-            raise Exception("Level Value not correct, try from 0 to 4.")
-
-        random.seed = self._seed
-
         while not self.stopped():
-            speed_right = speed_left = maximum_speed
-            if random.random() <= epsilon:
-                speed_right = random.uniform(-turning_speed, turning_speed)
-                speed_left = random.uniform(-turning_speed, turning_speed)
-                for i in range(3):
-                    self._robot.move(left=speed_right, right=speed_left, millis=200)
-            self._robot.move(left=speed_right, right=speed_left, millis=200)
+            self._robot.move(left=20.0, right=-10.0, millis=200)
             sensors = self._sensor_better_reading(self._robot.read_irs())
-            # self.log.debug(sensors)
-            # print(sensors)
-            if sum(sensors) != 0:
-                index_max_value = sensors.index(max(sensors))
-                if index_max_value == 5:
-                    # central -> turn
-                    if random.random() <= 0.5:
-                        while sum(sensors[3:5]) > 500:
-                            self._robot.move(left=-10.0, right=20.0, millis=500)
-                            sensors = self._sensor_better_reading(self._robot.read_irs())
-                    else:
-                        while sum(sensors[6:8]) > 500:
-                            self._robot.move(left=20.0, right=-10, millis=500)
-                            sensors = self._sensor_better_reading(self._robot.read_irs())
-                elif index_max_value == 3 or index_max_value == 4:
-                    # front right right -> go left
-                    while sum(sensors[3:5]) > 500:
-                        self._robot.move(left=-10.0, right=20.0, millis=200)
-                        sensors = self._sensor_better_reading(self._robot.read_irs())
-                elif index_max_value == 7 or index_max_value == 6:
-                    # front left left -> go right
-                    while sum(sensors[6:8]) > 500:
-                        self._robot.move(left=20.0, right=-10.0, millis=200)
-                        sensors = self._sensor_better_reading(self._robot.read_irs())
+            print('moved, sensors: ', sensors)
